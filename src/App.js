9b8bf06cafd922640de453e01f00a008530a6c1e
peterdyer7 <peter_dyer@hotmail.com>
2019-01-22 15:19:05 -0500

    init commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,95 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import gql from 'graphql-tag';
+import { Query, Mutation } from 'react-apollo';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+const LIST_TODOS_QUERY = gql`
+  query ListTodos {
+    listTodos {
+      id
+      name
+    }
   }
+`;
+
+function TodoList() {
+  return (
+    <Query
+      query={LIST_TODOS_QUERY}
+      // pollInterval={5000}
+    >
+      {({ loading, error, data }) => {
+        if (loading) return <p>Loading...</p>;
+        if (error) return <p>Error!</p>;
+        return (
+          <ul>
+            {data.listTodos.map((todo) => (
+              <li key={todo.id}>
+                {todo.name} - {todo.id < 0 ? 'optimistic' : 'real'}
+              </li>
+            ))}
+          </ul>
+        );
+      }}
+    </Query>
+  );
 }
 
-export default App;
+function TodoAdd() {
+  const handleKeyPress = (e, addTodo) => {
+    if (e.keyCode === 13) {
+      addTodo({
+        variables: { input: { name: e.target.value } },
+        optimisticResponse: {
+          addTodo: {
+            name: e.target.value,
+            id: Math.round(Math.random() * -1000000),
+            __typename: 'Todo'
+          }
+        },
+        update: (cache, { data: { addTodo } }) => {
+          const cachedTodos = cache.readQuery({
+            query: LIST_TODOS_QUERY
+          });
+          cachedTodos.listTodos.push(addTodo);
+          cache.writeQuery({
+            query: LIST_TODOS_QUERY,
+            data: cachedTodos
+          });
+        }
+        //refetchQueries: ['ListTodos']
+      });
+      e.target.value = '';
+    }
+  };
+
+  return (
+    <Mutation
+      mutation={gql`
+        mutation AddTodo($input: TodoInput) {
+          addTodo(input: $input) {
+            id
+            name
+          }
+        }
+      `}
+    >
+      {(addTodo, { data }) => (
+        <input
+          type="text"
+          placeholder="New todo..."
+          onKeyUp={(e) => handleKeyPress(e, addTodo)}
+        />
+      )}
+    </Mutation>
+  );
+}
+
+export default function App() {
+  return (
+    <>
+      <h1>Opti App</h1>
+      <TodoAdd />
+      <TodoList />
+    </>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloClient } from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { onError } from 'apollo-link-error';
+import { ApolloLink } from 'apollo-link';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+  link: ApolloLink.from([
+    onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors)
+        graphQLErrors.map(({ message, locations, path }) =>
+          console.log(
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+          )
+        );
+      if (networkError) console.log(`[Network error]: ${networkError}`);
+    }),
+    new HttpLink({
+      uri: 'http://localhost:4000/graphql',
+      credentials: 'same-origin'
+    })
+  ]),
+  cache: new InMemoryCache()
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
